678eb9f1e3a438c66527e18894d574f6d52c0f83
sushmamartha <marthasushma5@gmail.com>
2024-12-04 14:57:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class DoublyLinkedList {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+   
+    public void insertIntoBeginning(int data) {
+        Node newNode = new Node(data);
+        if (head != null) {
+            newNode.next = head;
+            head.prev = newNode;
+        }
+        head = newNode;
+    }
+
+   
+    public void printAllNodes() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+public class Main{
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertIntoBeginning(100);
+        list.insertIntoBeginning(200);
+        list.insertIntoBeginning(300);
+        list.printAllNodes();
+    }
+}
